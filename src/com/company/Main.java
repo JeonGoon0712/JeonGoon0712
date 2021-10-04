8f0584202356990615e6aedd7e0b43de1540cac2
JeonGoon <jeongoon0712@naver.com>
2021-10-04 14:26:11 +0900

    N 10926

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -10,29 +10,11 @@ import java.util.StringTokenizer;
 public class Main {
     public static void main(String args[]) throws IOException {
 
-        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        String name;
+        Scanner sc = new Scanner(System.in);
+        name  = sc.next();
 
-        Integer.parseInt(br.readLine());
+        System.out.println(""+name+"??!");
 
-        StringTokenizer st=new StringTokenizer(br.readLine()," ");
-
-        int max=-1000001;
-        int min=1000001;
-
-        while(st.hasMoreTokens()) {
-
-            int val=Integer.parseInt(st.nextToken());
-            if(max<val) {
-                max=val;
-
-            }
-            if(min>val) {
-                min=val;
-
-            }
-
-        }
-        System.out.println(min+" "+max);
-        
     }
 }
