1d459e32573823762751c4306be2c24e34459ed6
JeonGoon <jeongoon0712@naver.com>
2021-10-04 14:01:29 +0900

    N 2557

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -10,15 +10,7 @@ import java.util.StringTokenizer;
 public class Main {
     public static void main(String args[]) throws IOException {
 
-        Scanner sc=new Scanner(System.in);
-        int result=0;
-        for(int i=0; i<5;i++){
-            int a=sc.nextInt();
-            result+=Math.pow(a,2);
-
-        }
-        System.out.println(result%10);
-        sc.close();
+        System.out.println("Hello World!");
 
     }
 }
