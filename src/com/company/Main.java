2406493efc84ec7296d25844b1b8f9866884406f
JeonGoon <jeongoon0712@naver.com>
2021-10-04 14:12:17 +0900

    N 5337

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -10,35 +10,8 @@ import java.util.StringTokenizer;
 public class Main {
     public static void main(String args[]) throws IOException {
 
-        Scanner sc=new Scanner(System. in);
-        int N1=0,N2=0,num1=0,num2=0;
-        N1=sc.nextInt();
-        N2=sc.nextInt();
-
-        num1=(N1+N2)/2;
-        num2=N1-num1;
-
-        if(num1<0)
-        {
-            System.out.println("-1");
-        }
-        else if (num2 < 0)
-        {
-            System.out.println("-1");
-
-        }
-        else if (num1 + num2 != N1)
-        {
-            System.out.println("-1");
-
-        }
-        else if(num1 - num2 != N2)
-        {
-            System.out.println("-1");
-
-        }
-        else
-
-            System.out.println(num1+" "+num2);
+        System.out.println(".  .   .");
+        System.out.println("|  | _ | _. _ ._ _  _");
+        System.out.println("|/\\|(/.|(_.(_)[ | )(/.");
     }
 }
