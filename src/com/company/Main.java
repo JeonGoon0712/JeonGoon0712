84c1ddb778602526a90d30625d479217cd588dfe
JeonGoon <jeongoon0712@naver.com>
2021-10-04 13:55:23 +0900

    N 1712

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,15 +1,30 @@
 package com.company;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
 
 public class Main {
     public static void main(String args[]) throws IOException {
 
-        Scanner sc=new Scanner(System.in);
-        String a=sc.next();
-        System.out.println(Integer.parseInt(a,16));
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st=new StringTokenizer(br.readLine());
+
+        int A=Integer.parseInt(st.nextToken());
+        int B=Integer.parseInt(st.nextToken());
+        int C=Integer.parseInt(st.nextToken());
+
+        if (C <= B) {
+            System.out.println("-1");
+        }
+        else {
+            System.out.println((A/(C-B))+1);
+
+        }
 
 
     }
