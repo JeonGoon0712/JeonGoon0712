990eea97b9647a4e0ad03ffe4f0c05e01a997b4c
JeonGoon <jeongoon0712@naver.com>
2021-10-04 13:39:38 +0900

    N 1152

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,11 +6,13 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int x = sc.nextInt();
-        int y = sc.nextInt();
-        int w = sc.nextInt();
-        int h = sc.nextInt();
+        String dan=sc.nextLine().trim();
 
-        System.out.println(Math.min(Math.min(w-x, x),Math.min(h-y, y)));
+        if(dan.isEmpty())
+            System.out.println("0");
+
+        else
+            System.out.println(dan.split(" ").length);
+        
     }
 }
