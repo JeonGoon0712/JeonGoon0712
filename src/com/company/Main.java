6bd95b626c51508b24b842260c54d9f4e565c8a9
JeonGoon <jeongoon0712@naver.com>
2021-10-04 14:10:29 +0900

    N 4299

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -10,17 +10,35 @@ import java.util.StringTokenizer;
 public class Main {
     public static void main(String args[]) throws IOException {
 
-        Scanner sc=new Scanner(System.in);
+        Scanner sc=new Scanner(System. in);
+        int N1=0,N2=0,num1=0,num2=0;
+        N1=sc.nextInt();
+        N2=sc.nextInt();
 
-        int a= sc.nextInt();
-        int b= sc.nextInt();
-        int c= sc.nextInt();
-        int d= sc.nextInt();
-        int e= sc.nextInt();
-        int f= sc.nextInt();
+        num1=(N1+N2)/2;
+        num2=N1-num1;
 
-        System.out.print((1-a)+" "+(1-b)+" "+(2-c)+" "+(2-d)+" "+(2-e)+" "+(8-f));
+        if(num1<0)
+        {
+            System.out.println("-1");
+        }
+        else if (num2 < 0)
+        {
+            System.out.println("-1");
 
+        }
+        else if (num1 + num2 != N1)
+        {
+            System.out.println("-1");
 
+        }
+        else if(num1 - num2 != N2)
+        {
+            System.out.println("-1");
+
+        }
+        else
+
+            System.out.println(num1+" "+num2);
     }
 }
