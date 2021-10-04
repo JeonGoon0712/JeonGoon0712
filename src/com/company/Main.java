888e5b32eef3ba7e3893f70b3084d180f76100fe
JeonGoon <jeongoon0712@naver.com>
2021-10-04 14:13:09 +0900

    N 5339

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -10,8 +10,16 @@ import java.util.StringTokenizer;
 public class Main {
     public static void main(String args[]) throws IOException {
 
-        System.out.println(".  .   .");
-        System.out.println("|  | _ | _. _ ._ _  _");
-        System.out.println("|/\\|(/.|(_.(_)[ | )(/.");
+        System.out.println("     /~\\");
+        System.out.println("    ( oo|");
+        System.out.println("    _\\=/_");
+        System.out.println("   /  _  \\");
+        System.out.println("  //|/.\\|\\\\");
+        System.out.println(" ||  \\ /  ||");
+        System.out.println("============");
+        System.out.println("|          |");
+        System.out.println("|          |");
+        System.out.println("|          |");
+
     }
 }
