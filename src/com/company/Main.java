f1a8046e8bd9d6b2ba64f91344a8ac554994571c
JeonGoon <jeongoon0712@naver.com>
2021-10-04 14:06:50 +0900

    N 2741

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -15,9 +15,9 @@ public class Main {
         int num1= sc.nextInt();
 
 
-        for(int i=1;i<10;i++){
+        for(int i=1;i<num1+1;i++){
 
-            System.out.println(num1+" * "+i+" = "+num1*i);
+            System.out.println(i);
         }
 
 
