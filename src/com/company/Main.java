639ede47acaa92a7bfe77edba188bfe4a9afd615
JeonGoon <jeongoon0712@naver.com>
2021-10-04 14:27:01 +0900

    N 10950

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -10,11 +10,21 @@ import java.util.StringTokenizer;
 public class Main {
     public static void main(String args[]) throws IOException {
 
-        String name;
-        Scanner sc = new Scanner(System.in);
-        name  = sc.next();
+        Scanner in = new Scanner(System.in);
 
-        System.out.println(""+name+"??!");
+        int c = in.nextInt();
+        int arr[] = new int[c];
+
+        for (int i = 0; i < c; i++) {
+            int a = in.nextInt();
+            int b = in.nextInt();
+            arr[i] = a + b;
+        }
+        in.close();
+
+        for (int k : arr) {
+            System.out.println(k);
+        }
 
     }
 }
