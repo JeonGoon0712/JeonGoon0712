ed307c3bcf88485e7da5165b71cd62663506b38e
JeonGoon <jeongoon0712@naver.com>
2021-10-04 14:29:13 +0900

    N 10998

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -10,20 +10,14 @@ import java.util.StringTokenizer;
 public class Main {
     public static void main(String args[]) throws IOException {
 
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-        StringTokenizer st;
-        String str;
+        Scanner sc=new Scanner(System.in);
 
-        while( (str=br.readLine()) != null ){
+        int x=sc.nextInt();
 
-            st = new StringTokenizer(str," ");
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
-            sb.append(a+b).append("\n");
+        int y=sc.nextInt();
 
-        }
-        System.out.print(sb);
+        System.out.println(x*y);
+
 
     }
 }
