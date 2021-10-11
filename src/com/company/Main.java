e048ae0a59234557fb765780ce237974e9003766
JeonGoon <jeongoon0712@naver.com>
2021-10-11 16:17:06 +0900

    N 7287

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -10,24 +10,11 @@ import java.util.StringTokenizer;
 public class Main {
     public static void main(String args[]) throws IOException {
 
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        StringTokenizer st;
-        StringBuilder sb = new StringBuilder();
+        System.out.println(44);
+        System.out.println("jhk0712");
 
-        while(true) {
 
-            st = new StringTokenizer(br.readLine()," ");
-            int A = Integer.parseInt(st.nextToken());
-            int B = Integer.parseInt(st.nextToken());
-
-            if(A==0 && B==0) {
-                break;
-            }
-            sb.append((A+B)).append('\n');
 
         }
-        System.out.println(sb);
-
-    }
 }
