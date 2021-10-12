32f5271fba16920272b0681add556940e90a7282
JeonGoon <jeongoon0712@naver.com>
2021-10-12 13:52:16 +0900

    N 5554

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -11,8 +11,18 @@ public class Main {
     public static void main(String args[]) throws IOException {
 
 
-        System.out.println(44);
-        System.out.println("jhk0712");
+        Scanner sc=new Scanner(System.in);
+        int A=sc.nextInt();
+        int B=sc.nextInt();
+        int C=sc.nextInt();
+        int D=sc.nextInt();
+
+        int Z=A+B+C+D;
+
+
+        System.out.println(Z/60);
+        System.out.println(Z%60);
+
 
 
 
