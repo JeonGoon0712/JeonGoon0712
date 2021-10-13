b1adf2dbc82cab1dcb909507cfcd7fac127e98fa
JeonGoon <jeongoon0712@naver.com>
2021-10-13 14:06:29 +0900

    N 9653

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -11,17 +11,17 @@ public class Main {
     public static void main(String args[]) throws IOException {
 
 
-        Scanner sc=new Scanner(System.in);
-        int A=sc.nextInt();
-        int B=sc.nextInt();
-        int C=sc.nextInt();
-        int D=sc.nextInt();
-
-        int Z=A+B+C+D;
-
-
-        System.out.println(Z/60);
-        System.out.println(Z%60);
+        System.out.println("    8888888888  888    88888\n" +
+                "   88     88   88 88   88  88\n" +
+                "    8888  88  88   88  88888\n" +
+                "       88 88 888888888 88   88\n" +
+                "88888888  88 88     88 88    888888\n" +
+                "\n" +
+                "88  88  88   888    88888    888888\n" +
+                "88  88  88  88 88   88  88  88\n" +
+                "88 8888 88 88   88  88888    8888\n" +
+                " 888  888 888888888 88  88      88\n" +
+                "  88  88  88     88 88   88888888");
 
 
 
