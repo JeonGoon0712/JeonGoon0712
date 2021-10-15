b475e6c5a86d0e29e9c3691a82024cd5115ffeb6
JeonGoon <jeongoon0712@naver.com>
2021-10-15 12:39:00 +0900

    N 10718

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -10,16 +10,8 @@ import java.util.StringTokenizer;
 public class Main {
     public static void main(String args[]) throws IOException {
 
-        Scanner sc=new Scanner(System.in);
-
-        int A=sc.nextInt();
-        int B=sc.nextInt();
-        int C=sc.nextInt();
-
-        System.out.println((A+B)%C);
-        System.out.println(((A%C) + (B%C))%C);
-        System.out.println((A*B)%C);
-        System.out.println( ((A%C)*(B%C))%C);
+        System.out.println("강한친구 대한육군");
+        System.out.println("강한친구 대한육군");
 
 
 
