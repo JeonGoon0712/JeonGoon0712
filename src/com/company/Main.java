d8656cd67296f9622eb518de0b5ffd40fba9d537
JeonGoon <jeongoon0712@naver.com>
2021-10-16 09:47:32 +0900

    N 15733

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -10,8 +10,7 @@ import java.util.StringTokenizer;
 public class Main {
     public static void main(String args[]) throws IOException {
 
-        System.out.println("강한친구 대한육군");
-        System.out.println("강한친구 대한육군");
+        System.out.println("I'm Sexy");
 
 
 
