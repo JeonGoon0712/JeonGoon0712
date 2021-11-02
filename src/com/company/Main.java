39783d9b2cb4fe12b73a9c1d54407a3d73faefd4
JeonGoon <jeongoon0712@naver.com>
2021-11-02 16:30:59 +0900

    N 1120

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,25 +1,27 @@
 package com.company;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.Scanner;
-import java.util.StringTokenizer;
 
 
 public class Main {
     public static void main(String args[]) throws IOException {
 
 
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        String b = sc.next();
+        int ans = a.length();
 
-        Scanner sc= new Scanner(System.in);
-        int A=sc.nextInt();
-        int B=sc.nextInt();
-
-        System.out.println((B-A)+" "+B);
-
-
-
+        for(int i=0; i<=b.length()-a.length(); i++) {
+            int cnt = 0;
+            for(int j=0; j<a.length(); j++) {
+                if(a.charAt(j) != b.charAt(i+j))
+                    cnt++;
+            }
+            ans = Math.min(cnt, ans);
+        }
+        System.out.println(ans);
 
         }
 }
